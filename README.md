8f6a4fab7ddc8c7cd401e0f2a37a6bec4a9d77c9
longmatthewh <longmatthewh@gmail.com>
2015-06-14 13:16:51 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+[ColorA11y v1.0.0](http://longmatthewh.github.io/) [![Build Status](https://travis-ci.org/longmatthewh/ColorA11y.svg?branch=master)](https://travis-ci.org/longmatthewh/ColorA11y) [![Built with Grunt](https://cdn.gruntjs.com/builtwith.png)](http://gruntjs.com/)
+===============
+
+Created and maintained by [Matt Long](https://github.com/longmatthewh).
+
+
+##License
+ColorA11y is licensed under the MIT License.
---
+++ test/js/specs/colora11ySpec.js
@@ -0,0 +1,44 @@
+describe('colora11y', function() {
+    it('ratio is 21:1 for black on white', function() {
+        var compliance = colora11y.calcContrastCompliance('#000000', '#ffffff');
+        expect(compliance.ratio).toBe('21:1');
+        expect(compliance.aa.normal.pass).toBe(true);
+        expect(compliance.aa.large.pass).toBe(true);
+        expect(compliance.aaa.normal.pass).toBe(true);
+        expect(compliance.aaa.large.pass).toBe(true);
+    });
+
+    it('ratio is 1:1 for black on black', function() {
+        var compliance = colora11y.calcContrastCompliance('#000000', '#000000');
+        expect(compliance.ratio).toBe('1:1');
+        expect(compliance.aa.normal.pass).toBe(false);
+        expect(compliance.aa.large.pass).toBe(false);
+        expect(compliance.aaa.normal.pass).toBe(false);
+        expect(compliance.aaa.large.pass).toBe(false);
+    });
+
+    it('ratio is 5.74:1 for grey on white', function() {
+        var compliance = colora11y.calcContrastCompliance('#666666', '#ffffff');
+        expect(compliance.ratio).toBe('5.74:1');
+        expect(compliance.aa.normal.pass).toBe(true);
+        expect(compliance.aa.large.pass).toBe(true);
+        expect(compliance.aaa.normal.pass).toBe(false);
+        expect(compliance.aaa.large.pass).toBe(true);
+    });
+
+    it('ratio is 3.54:1 for lighter grey on white', function() {
+        var compliance = colora11y.calcContrastCompliance('#888888', '#ffffff');
+        expect(compliance.ratio).toBe('3.54:1');
+        expect(compliance.aa.normal.pass).toBe(false);
+        expect(compliance.aa.large.pass).toBe(true);
+        expect(compliance.aaa.normal.pass).toBe(false);
+        expect(compliance.aaa.large.pass).toBe(false);
+    });
+
+    it('works with 3 digit hex', function() {
+        expect(colora11y.calcContrastCompliance('#000', '#fff').ratio).toBe('21:1');
+        expect(colora11y.calcContrastCompliance('#000', '#000').ratio).toBe('1:1');
+        expect(colora11y.calcContrastCompliance('#666', '#fff').ratio).toBe('5.74:1');
+        expect(colora11y.calcContrastCompliance('#888', '#fff').ratio).toBe('3.54:1');
+    });
+});
